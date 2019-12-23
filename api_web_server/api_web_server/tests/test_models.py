96208f8e1ab27f02c8cbde13a45382392486ddcb
Rustam <rustam.aliev.work@yandex.ru>
2019-12-23 21:12:44 +0300

    Dummy for models testing has been added.

---
+++ api_web_server/api_web_server/tests/test_models.py
@@ -0,0 +1,14 @@
+import unittest
+
+import mock
+from Models import Places, Events
+
+
+class ModelsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
