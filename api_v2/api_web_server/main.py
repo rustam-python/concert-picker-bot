7ccccd255af4648000ba2d7b40b0c5cf3eadb6d7
Rustam <rustam.aliev.work@yandex.ru>
2020-10-01 11:08:30 +0300

    Color logger class has been added.

---
+++ api_v2/api_web_server/main.py
@@ -0,0 +1,35 @@
+from logging import Formatter, CRITICAL, ERROR, WARNING, INFO, DEBUG, StreamHandler, getLogger
+
+
+class CustomFormatter(Formatter):
+    """Logging Formatter to add colors"""
+    bold = '\x1b[1m'
+    italic = '\x1b[3m'
+    underline = '\x1b[4m'
+    reverse_video = '\x1b[7m'
+    crossed_out = '\x1b[9m'
+
+    red = '\x1b[31m'
+    green = '\x1b[32m'
+    yellow = '\x1b[33m'
+    blue = '\x1b[34m'
+    magenta = '\x1b[35m'
+    cyan = '\x1b[36m'
+    white = '\x1b[37m'
+    black = '\x1b[30m'
+
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        DEBUG: italic + white + format + reset,
+        INFO: cyan + format + reset,
+        WARNING: yellow + format + reset,
+        ERROR: red + format + reset,
+        CRITICAL: bold + red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = Formatter(log_fmt)
+        return formatter.format(record)
