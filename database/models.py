ece1e3281392b19320064e41a08d12701464d787
rustam-python <rustam.aliev.work@yandex.ru>
2021-07-28 15:05:50 +0300

    New check DB models were added.

--- database/models.py
+++ database/models.py
@@ -121,3 +121,54 @@ class EventDates(BaseModel):
     @classmethod
     def add(cls, event_id: int, date_start: datetime.datetime, date_stop: datetime.datetime) -> 'EventDates':
         return cls.get_or_create(event_id=event_id, date_start=date_start, date_stop=date_stop)[0]
+
+
+D = typing.TypeVar('D', bound='_DictionaryModel')
+
+
+class _DictionaryModel(BaseModel):
+    """
+    Base class for models that describe DB dictionary tables
+    """
+    migration_priority = 0
+
+    id = AutoField()
+    name = CharField(unique=True)
+
+    @classmethod
+    def add(cls: typing.Type[D], name: str) -> 'D':
+        return cls.get_or_create(name=name)[0]
+
+
+class Artist(_DictionaryModel):
+    pass
+
+
+class Album(_DictionaryModel):
+    pass
+
+
+class Track(_DictionaryModel):
+    pass
+
+
+class Scrobble(BaseModel):
+    migration_priority = 1
+
+    id = AutoField()
+    artist = ForeignKeyField(Artist)
+    album = ForeignKeyField(Album)
+    track = ForeignKeyField(Track)
+    scrobble_date = DateTimeField(unique=True)
+
+    @classmethod
+    def add(cls, artist: str, album: str, track: str, scrobble_date: datetime.datetime) -> 'Scrobble':
+        try:
+            return cls.get(cls.scrobble_date == scrobble_date)
+        except cls.DoesNotExist:
+            return cls.create(
+                track=Track.add(track),
+                artist=Artist.add(artist),
+                album=Album.add(album),
+                scrobble_date=scrobble_date
+            )
